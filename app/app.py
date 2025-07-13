b017bff340e56c80bac0cd28dc812e56cd216b96
Axel <axel.game.programmer@gmail.com>
2025-07-13 14:02:18 +0200

    Testing new pipeline

--- app/app.py
+++ app/app.py
@@ -42,118 +42,137 @@ def calculate_smoothed_contours(image, config_values):
     return smooth_contours
 
 
+def make_all_transforms_image(kinect, config, depth_image, previous_min_depth, previous_max_depth, previous_depth=None):
+    # GET PRINCIPAL APPLICATION VALUES
+    config_values = config.get_values()
+
+    # REMOVE ERRORS IN IMAGE
+    depth_image_no_zeros = ImageTransformerDepth.remove_zeros(image=depth_image)
+
+    # # APPLY CAMERA FOCUS
+    # depth_image_transformed = kinect.apply_camera_focus(kinect_frame=KinectFrames.DEPTH,
+    #                                                     image=depth_image_no_zeros)
+    depth_image_transformed = depth_image_no_zeros
+
+    if previous_depth is None or config_values[ConfigControllerEnum.RESET_IMAGE.name] or previous_min_depth != \
+            config_values[ConfigControllerEnum.MIN_DEPTH.name] or previous_max_depth != config_values[
+        ConfigControllerEnum.MAX_DEPTH.name]:
+        previous_min_depth = config_values[ConfigControllerEnum.MIN_DEPTH.name]
+        previous_max_depth = config_values[ConfigControllerEnum.MAX_DEPTH.name]
+        config.set_value(key=ConfigControllerEnum.RESET_IMAGE.name, value=False)
+
+        last_depth_image = ImageTransformerDepth.set_data_between_distance(image=depth_image_transformed,
+                                                                           min_depth=config_values[
+                                                                               ConfigControllerEnum.MIN_DEPTH.name],
+                                                                           max_depth=config_values[
+                                                                               ConfigControllerEnum.MAX_DEPTH.name])
+    else:
+        # COMBINE WITH PREVIOUS IMAGE DATA OUT OF THE SANDBOX RANGE (MIN AND MAX DEPTH)
+        last_depth_image, mask_with_neighbors = ImageTransformerDepth.remove_data_between_distance_neighbors(
+            image=depth_image_transformed,
+            min_depth=config_values[ConfigControllerEnum.MIN_DEPTH.name],
+            max_depth=config_values[ConfigControllerEnum.MAX_DEPTH.name],
+            other_image=previous_depth, iterations=30)
+
+        # REMOVE NOISE (CHANGES < 5 MM)
+        condition = (mask_with_neighbors == 0) & (np.abs(last_depth_image - previous_depth) < config_values[
+            ConfigControllerEnum.ERRORS_UMBRAL.name])
+        value = previous_depth
+        depth_image_no_errors = ImageTransformerDepth.apply_mask(image=last_depth_image, condition=condition,
+                                                                 value=value)
+
+        # COMBINE MEDIUM NOISE (CHANGES BETWEEN 5MM AND 15MM) --> 10% ACTUAL, 90% PREVIOUS
+        condition = (
+                (mask_with_neighbors == 0) &
+                (config_values[ConfigControllerEnum.ERRORS_UMBRAL.name] <= np.abs(
+                    depth_image_no_errors - previous_depth)) &
+                (np.abs(depth_image_no_errors - previous_depth) <= config_values[
+                    ConfigControllerEnum.MEDIUM_NOISE.name])
+        )
+        value = previous_depth * 0.9 + depth_image_no_errors * 0.1
+        depth_image_no_noise = ImageTransformerDepth.apply_mask(image=depth_image_no_errors,
+                                                                condition=condition,
+                                                                value=value)
+
+        # REMOVE BIG NOISE (CHANGES BETWEEN 15MM AND 30MM) --> 50% ACTUAL, 50% PREVIOUS
+        condition = (
+                (mask_with_neighbors == 0) &
+                (config_values[ConfigControllerEnum.MEDIUM_NOISE.name] <= np.abs(
+                    depth_image_no_noise - previous_depth)) &
+                (np.abs(depth_image_no_noise - previous_depth) <= config_values[
+                    ConfigControllerEnum.BIG_NOISE.name])
+        )
+        value = previous_depth * 0.5 + depth_image_no_noise * 0.5
+        last_depth_image = ImageTransformerDepth.apply_mask(image=depth_image_no_noise, condition=condition,
+                                                            value=value)
+
+    # SAVE PREVIOUS IMAGE
+    previous_depth = ImageTransformerDepth.duplicate(image=last_depth_image)
+
+    # APPLY CAMERA FOCUS
+    depth_image_transformed = kinect.apply_camera_focus(kinect_frame=KinectFrames.DEPTH,
+                                                        image=depth_image_no_zeros)
+
+    # NORMALIZE IMAGE
+    depth_image_normalized = ImageTransformerDepth.normalize_between_distance(image=depth_image_transformed,
+                                                                              min_depth=config_values[
+                                                                                  ConfigControllerEnum.MIN_DEPTH.name],
+                                                                              max_depth=config_values[
+                                                                                  ConfigControllerEnum.MAX_DEPTH.name])
+
+    # TRANSFORM IMAGE TO UINT8
+    depth_image_uint8 = ImageTransformerDepth.transform_dtype(image=depth_image_normalized, dtype=np.uint8)
+
+    # BLURRED IMAGE FOR CALCULATIONS
+    depth_image_blurred = ImageTransformerDepth.degaussing(image=depth_image_uint8, ksize=(11, 11), sigma_x=0)
+
+    # CONTOURS (LEVEL LINES)
+    smoothed_contours = calculate_smoothed_contours(image=depth_image_blurred, config_values=config_values)
+
+    # GENERATE COLOR IMAGE (INVERT + APPLY COLORMAP)
+    depth_image_uint8_inverted = ImageTransformerDepth.invert(image=depth_image_uint8)
+    colormap_image = ImageTransformerDepth.apply_colormap(image=depth_image_uint8_inverted,
+                                                          colormap=config_values[
+                                                              ConfigControllerEnum.COLORMAP.name])
+
+    # DRAW INFORMATION IN COLORMAP IMAGE
+    colormap_with_contours = ImageTransformerDepth.draw_contours(image=colormap_image, thickness=1,
+                                                                 contours=smoothed_contours, color=(0, 0, 0))
+
+    # UPDATE IMAGE PROJECTED
+    final_image = colormap_with_contours
+    return final_image, previous_depth, previous_min_depth, previous_max_depth
+
+
 def projector_application(projector_screen, kinect, config: SharedConfig):
     previous_min_depth = config.get_value(ConfigControllerEnum.MIN_DEPTH.name)
     previous_max_depth = config.get_value(ConfigControllerEnum.MAX_DEPTH.name)
 
     # GET FIRST DEPHT IMAGE FOR COMBINED IMAGES IN PROCESS
     depth_image = kinect.get_image_calibrate(kinect_frame=KinectFrames.DEPTH, avoid_camera_focus=True)
-    depth_image_without_zeros = ImageTransformerDepth.remove_zeros(image=depth_image)
-    depth_image_set_distance_datas = ImageTransformerDepth.set_data_between_distance(image=depth_image_without_zeros,
-                                                                                     min_depth=previous_min_depth,
-                                                                                     max_depth=previous_max_depth)
-
-    previous_depth = depth_image_set_distance_datas
+    final_image, previous_depth, previous_min_depth, previous_max_depth = make_all_transforms_image(
+        kinect=kinect,
+        config=config,
+        depth_image=depth_image,
+        previous_min_depth=previous_min_depth,
+        previous_max_depth=previous_max_depth)
 
     # CREATE WINDOW SCREEN
-    projector_screen.create_window_calibrate(window_name="Projector Window", image=previous_depth, fullscreen=True)
+    projector_screen.create_window_calibrate(window_name="Projector Window", image=final_image, fullscreen=True)
     while projector_screen.check_if_window_active(window_name="Projector Window"):
         depth_image = kinect.get_image_calibrate(kinect_frame=KinectFrames.DEPTH, avoid_camera_focus=True)
         if depth_image is not None:
-            config_values = config.get_values()
+            final_image, previous_depth, previous_min_depth, previous_max_depth = make_all_transforms_image(
+                kinect=kinect,
+                config=config,
+                depth_image=depth_image,
+                previous_min_depth=previous_min_depth,
+                previous_max_depth=previous_max_depth,
+                previous_depth=previous_depth)
 
-            if (config_values[ConfigControllerEnum.RESET_IMAGE.name] or
-                    (previous_min_depth != config_values[ConfigControllerEnum.MIN_DEPTH.name] or
-                     previous_max_depth != config_values[ConfigControllerEnum.MAX_DEPTH.name])):
-                depth_image_set_distance_datas = ImageTransformerDepth.set_data_between_distance(
-                    image=depth_image_without_zeros,
-                    min_depth=config_values[ConfigControllerEnum.MIN_DEPTH.name],
-                    max_depth=config_values[ConfigControllerEnum.MAX_DEPTH.name])
-
-                previous_depth = depth_image_set_distance_datas
-                previous_min_depth = config_values[ConfigControllerEnum.MIN_DEPTH.name]
-                previous_max_depth = config_values[ConfigControllerEnum.MAX_DEPTH.name]
-                config.set_value(key=ConfigControllerEnum.RESET_IMAGE.name, value=False)
-
-            # REMOVE ERRORS IN IMAGE
-            depth_image_no_zeros = ImageTransformerDepth.remove_zeros(image=depth_image)
-
-            # COMBINE WITH PREVIOUS IMAGE DATA OUT OF THE SANDBOX RANGE (MIN AND MAX DEPTH)
-            last_depth_image, mask_with_neighbors = ImageTransformerDepth.remove_data_between_distance_neighbors(
-                image=depth_image_no_zeros,
-                min_depth=config_values[ConfigControllerEnum.MIN_DEPTH.name],
-                max_depth=config_values[ConfigControllerEnum.MAX_DEPTH.name],
-                other_image=previous_depth, iterations=30)
-
-            # REMOVE NOISE (CHANGES < 5 MM)
-            condition = (mask_with_neighbors == 0) & (np.abs(last_depth_image - previous_depth) < config_values[
-                ConfigControllerEnum.ERRORS_UMBRAL.name])
-            value = previous_depth
-            depth_image_no_errors = ImageTransformerDepth.apply_mask(image=last_depth_image, condition=condition,
-                                                                     value=value)
-
-            # COMBINE MEDIUM NOISE (CHANGES BETWEEN 5MM AND 15MM) --> 10% ACTUAL, 90% PREVIOUS
-            condition = (
-                    (mask_with_neighbors == 0) &
-                    (config_values[ConfigControllerEnum.ERRORS_UMBRAL.name] <= np.abs(
-                        depth_image_no_errors - previous_depth)) &
-                    (np.abs(depth_image_no_errors - previous_depth) <= config_values[
-                        ConfigControllerEnum.MEDIUM_NOISE.name])
-            )
-            value = previous_depth * 0.9 + depth_image_no_errors * 0.1
-            depth_image_no_noise = ImageTransformerDepth.apply_mask(image=depth_image_no_errors,
-                                                                    condition=condition,
-                                                                    value=value)
-
-            # REMOVE BIG NOISE (CHANGES BETWEEN 15MM AND 30MM) --> 50% ACTUAL, 50% PREVIOUS
-            condition = (
-                    (mask_with_neighbors == 0) &
-                    (config_values[ConfigControllerEnum.MEDIUM_NOISE.name] <= np.abs(
-                        depth_image_no_noise - previous_depth)) &
-                    (np.abs(depth_image_no_noise - previous_depth) <= config_values[
-                        ConfigControllerEnum.BIG_NOISE.name])
-            )
-            value = previous_depth * 0.5 + depth_image_no_noise * 0.5
-            last_depth_image = ImageTransformerDepth.apply_mask(image=depth_image_no_noise, condition=condition,
-                                                                value=value)
-
-            # SAVE PREVIOUS IMAGE
-            previous_depth = ImageTransformerDepth.duplicate(image=last_depth_image)
-
-            # APPLY CAMERA FOCUS
-            depth_image_transformed = kinect.apply_camera_focus(kinect_frame=KinectFrames.DEPTH,
-                                                                image=last_depth_image)
-
-            # NORMALIZE IMAGE
-            depth_image_normalized = ImageTransformerDepth.normalize_between_distance(image=depth_image_transformed,
-                                                                                      min_depth=config_values[
-                                                                                          ConfigControllerEnum.MIN_DEPTH.name],
-                                                                                      max_depth=config_values[
-                                                                                          ConfigControllerEnum.MAX_DEPTH.name])
-
-            # TRANSFORM IMAGE TO UINT8
-            depth_image_uint8 = ImageTransformerDepth.transform_dtype(image=depth_image_normalized, dtype=np.uint8)
-
-            # BLURRED IMAGE FOR CALCULATIONS
-            depth_image_blurred = ImageTransformerDepth.degaussing(image=depth_image_uint8, ksize=(11, 11), sigma_x=0)
-
-            # CONTOURS (LEVEL LINES)
-            smoothed_contours = calculate_smoothed_contours(image=depth_image_blurred, config_values=config_values)
-
-            # GENERATE COLOR IMAGE (INVERT + APPLY COLORMAP)
-            depth_image_uint8_inverted = ImageTransformerDepth.invert(image=depth_image_uint8)
-            colormap_image = ImageTransformerDepth.apply_colormap(image=depth_image_uint8_inverted,
-                                                                  colormap=config_values[
-                                                                      ConfigControllerEnum.COLORMAP.name])
-
-            # DRAW INFORMATION IN COLORMAP IMAGE
-            colormap_with_contours = ImageTransformerDepth.draw_contours(image=colormap_image, thickness=1,
-                                                                         contours=smoothed_contours, color=(0, 0, 0))
-
-            # UPDATE IMAGE PROJECTED
-            final_image = colormap_with_contours
             projector_screen.update_window_image_calibrate(window_name="Projector Window", image=final_image)
 
+            # UPDATE PRINCIPAL INTERFACE
             rgb_image = kinect.get_image_calibrate(kinect_frame=KinectFrames.COLOR, avoid_camera_matrix=True)
             with config.lock:
                 config.current_image = final_image
